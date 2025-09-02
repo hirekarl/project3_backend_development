c13d96e5dd298c276934ece4009e35c75c96388b
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-02 10:42:46 -0400

    Implemented authMiddleware.

--- taskmaster/utils/auth.js
+++ taskmaster/utils/auth.js
@@ -1,4 +1,25 @@
-const authMiddleware = (req, res, next) => {}
+const jwt = require("jsonwebtoken")
+
+const { JWT_SECRET, JWT_EXPIRY } = require("./index")
+
+const authMiddleware = (req, res, next) => {
+  let token = req.headers.authorization
+
+  if (token) {
+    token = token.split(" ").pop().trim()
+  } else {
+    return res.sendStatus(401)
+  }
+
+  try {
+    const { data } = jwt.verify(token, JWT_SECRET, { maxAge: JWT_EXPIRY })
+    req.user = data
+  } catch (error) {
+    console.error("Invalid token:", error)
+  }
+
+  next()
+}
 
 const userIsOwner = (req, res, next) => {}
 
--- taskmaster/utils/index.js
+++ taskmaster/utils/index.js
@@ -4,5 +4,6 @@ require("dotenv").config(path.resolve(__dirname, "../.env"))
 const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI
 const JWT_SECRET = process.env.JWT_SECRET
+const JWT_EXPIRY = "2h"
 
-module.exports = { PORT, MONGO_URI, JWT_SECRET }
+module.exports = { PORT, MONGO_URI, JWT_SECRET, JWT_EXPIRY }
