83baa03ac0ae10d4950c0f440c42ec375413a05d
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-09-02 14:08:30 -0400

    Removed __v property from toJSON for Project and Task.

--- taskmaster/models/Project.js
+++ taskmaster/models/Project.js
@@ -1,21 +1,31 @@
 const mongoose = require("mongoose")
 const { Schema, model } = mongoose
 
-const projectSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const projectSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-})
+  {
+    toJSON: {
+      transform: (_doc, ret) => {
+        delete ret.__v
+        return ret
+      },
+    },
+  }
+)
 
 mongoose.set("runValidators", true)
 
--- taskmaster/models/Task.js
+++ taskmaster/models/Task.js
@@ -3,26 +3,36 @@ const { Schema, model } = mongoose
 
 const taskStatuses = ["To Do", "In Progress", "Done"]
 
-const taskSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: taskStatuses,
+      default: "To Do",
+    },
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: taskStatuses,
-    default: "To Do",
-  },
-  project: {
-    type: Schema.Types.ObjectId,
-    ref: "Project",
-    required: true,
-  },
-})
+  {
+    toJSON: {
+      transform: (_doc, ret) => {
+        delete ret.__v
+        return ret
+      },
+    },
+  }
+)
 
 mongoose.set("runValidators", true)
 
